5c85424c1ad6ae8888ae59fb1fd720fd6f29173b
sheldon <sheldon.jf.huang@pccw.com>
2022-11-24 10:29:49 +0800

    update effect schduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -10,40 +10,41 @@
 import { createDeps } from './deps'
 class ReactiveEffect {
     private _fn: Function
-
-    constructor(fn: Function,public scheduler?) {
+    private active: boolean
+    options: {}
+    constructor(fn: Function, public scheduler?) {
         this._fn = fn
+        this.active = false
         this.scheduler = scheduler
+        this.options = {}
     }
-
     run() {
         activeEffect = this
-        //cleanupEffect(this)
         return this._fn()
     }
-
+    stop() {
+        if (this.active) {
+            cleanupEffect(this)
+            this.active = false
+        }
+    }
 }
 
 let targetMap = new WeakMap()
-
 let activeEffect: any;
 
 export function track(target: object, key: PropertyKey) {
     if (!activeEffect) return
     let depsMap = targetMap.get(target)
-
     if (!depsMap) {
         depsMap = new Map()
         targetMap.set(target, depsMap)
     }
-
     let dep = depsMap.get(key)
-
     if (!dep) {
         dep = createDeps()
         depsMap.set(key, dep)
     }
-
     dep.add(activeEffect)
     activeEffect.deps = dep
 }
@@ -54,12 +55,11 @@ export function trigger(target: object, key: PropertyKey) {
     let dep = depsMap.get(key);
     deps.push(...dep)
     for (let effectFn of deps) {
-        if(effectFn.scheduler){
+        if (effectFn.scheduler) {
             effectFn.scheduler()
-        }else{
+        } else {
             effectFn.run()
         }
-        
     }
 }
 
@@ -69,12 +69,10 @@ function cleanupEffect(effect) {
     })
 }
 
-export function effect(fn: Function,options:any={} ) {
-    const scheduler = options.scheduler
+export function effect(fn: Function, options: any = {}) {
 
-    
-    const _effect = new ReactiveEffect(fn,scheduler)
-    
+    const _effect = new ReactiveEffect(fn, options.scheduler)
+    Object.assign(_effect, options)
     _effect.run()
     return _effect.run.bind(_effect)
 }
