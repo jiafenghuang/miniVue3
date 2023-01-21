c34cb9333def19b18d9b40e3297c5b720039b92e
Sheldon <374734987@qq.com>
2023-01-21 19:27:23 +0800

    update shared file format

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,19 +1,18 @@
-const getType = (value: any) => {
-    return Object.prototype.toString.call(value).slice(8, -1)
-}
+const getType = (val) => Object.prototype.toString.call(val).slice(8, -1)
 
-export const isObject = (value: any) => value !== null && typeof value === "object"
+export const isObject = (val) => val !== null && typeof val === "object"
+
+export const hasChanged = (val, newVal) => !Object.is(val, newVal)
+
+export const isOn = (key:string) => /^on[A-Z]/.test(key)
+
+export const hasOwn = (Obj, key) => Object.prototype.hasOwnProperty.call(Obj, key)
+
+export const capitalize = (str:string) => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`
+
+export const toHandlerKey = (str:string) => str ? "on" + capitalize(str) : "" 
+
+export const camelize = (str:string) => str.replace(/-(\w)/,(_,v)=>(v && v.toUpperCase()) ?? "")
+
 
 export const extend = Object.assign
-
-export const hasChanged = (val, newVal) => {
-    return !Object.is(val, newVal)
-}
-export const isOn=( key:string )=> /^on[A-Z]/.test(key)
-export const hasOwn =(Obj, key)=> Object.prototype.hasOwnProperty.call(Obj, key)
-
-export const capitalize = (string:string)=> string.charAt(0).toUpperCase() + string.slice(1) // add=> Add
-export const toHandlerKey = (string:string)=> string ? "on" + capitalize(string) : "" 
-export const camelize = (string:string)=> string.replace(/-(\w)/,(_,v)=>{
-  return v ? v.toUpperCase() : ""
-})
