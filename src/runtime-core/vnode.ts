faeaaf2d89e6d7c9cfa87f95049116413a6c4997
Sheldon <374734987@qq.com>
2023-01-22 22:45:14 +0800

    add createTextVNode

--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,6 +1,7 @@
 import { ShapeFlags } from '../shared/ShapeFlags';
 
 export const Fragment = Symbol('Fragment');
+export const Text = Symbol('Text');
 
 export function createVNode(type, props?, children?) {
 	const vnode = {
@@ -29,4 +30,8 @@ export function createVNode(type, props?, children?) {
 
 function getShapeFlag(type) {
 	return typeof type === 'string' ? ShapeFlags.ELEMENT : ShapeFlags.STATEFUL_COMPONENT;
+}
+
+export function createTextVNode(text: string) {
+	return createVNode(Text, {}, text);
 }
